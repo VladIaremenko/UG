5d46485b2ae12b3f4fb3375937193061b9cdc94e
Vlad <vlad.iaremenko@gmail.com>
2023-03-12 17:28:10 +0300

    Subway updates

---
+++ Assets/Scripts/_Algo/Route.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGA.Assets.Scripts._Algo
+{
+    public class Route : MonoBehaviour
+    {
+        [SerializeField] private List<Station> _stations;
+
+        internal void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/_Algo/Station.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGA.Assets.Scripts._Algo
+{
+    public class Station : MonoBehaviour
+    {
+        [SerializeField] private List<Station> _connectedStations;
+        [SerializeField] private List<LineRenderer> _renderers;
+
+        internal void Init()
+        {
+            _renderers.Clear();
+
+            foreach (var item in _connectedStations)
+            {
+                var renderer = gameObject.AddComponent<LineRenderer>();
+
+                renderer.positionCount = 2;
+
+                renderer.SetPosition(0, transform.position);
+                renderer.SetPosition(0, item.transform.position);
+
+                _renderers.Add(renderer);
+            }            
+        }
+    }
+}
+
+
