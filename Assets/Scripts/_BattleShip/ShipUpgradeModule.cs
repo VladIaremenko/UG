2884eb8a778c2d44f7340891b9d3f6cb2c88f504
Vlad <vlad.iaremenko@gmail.com>
2023-03-15 16:31:24 +0300

    Refractor

---
+++ Assets/Scripts/_BattleShip/ShipUpgradeModule.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UGA.Assets.Scripts._BattleShip
+{
+    [CreateAssetMenu(fileName = "ShipUpgradeModule", menuName = "SO/Ship/Module/ShipUpradeModule", order = 1)]
+    public class ShipUpgradeModule : ShipModuleData
+    {
+    }
+}
+
+
---
+++ Assets/Scripts/_BattleShip/ShipWeaponModule.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UGA.Assets.Scripts._BattleShip
+{
+    [CreateAssetMenu(fileName = "ShipWeaponModule", menuName = "SO/Ship/Module/ShipWeaponModule", order = 1)]
+    public class ShipWeaponModule : ShipModuleData
+    {
+    }
+}
+
+
